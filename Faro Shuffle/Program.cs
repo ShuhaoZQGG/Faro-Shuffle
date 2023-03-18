fef2acab76a44909d236f36c7c3c11a722bfd5d6
Shuhao Zhang <shuhaozhangchris@gmail.com>
2023-03-18 16:38:51 -0400

    print all possible cards; using yield return in IEnummerable

--- Faro Shuffle/Program.cs
+++ Faro Shuffle/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+// See https://aka.ms/new-console-template for more information
+var startingDeck = from s in Suits()
+                   from r in Ranks()
+                   select new { Suit = s, Rank = r };
+foreach(var card in startingDeck)
+{
+    Console.WriteLine(card);
+}
+static IEnumerable<string> Suits()
+{
+    yield return "clubs";
+    yield return "diamonds";
+    yield return "hearts";
+    yield return "spades";
+}
+
+static IEnumerable<string> Ranks()
+{
+    yield return "two";
+    yield return "three";
+    yield return "four";
+    yield return "five";
+    yield return "six";
+    yield return "seven";
+    yield return "eight";
+    yield return "nine";
+    yield return "ten";
+    yield return "jack";
+    yield return "queen";
+    yield return "king";
+    yield return "ace";
+}
