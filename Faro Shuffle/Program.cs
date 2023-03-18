ff51ba056803f579552688aa83ee0a64bcd2f466
Shuhao Zhang <shuhaozhangchris@gmail.com>
2023-03-18 17:00:54 -0400

    update gitignore and move it to the correct location

--- Faro Shuffle/Program.cs
+++ Faro Shuffle/Program.cs
@@ -2,10 +2,26 @@
 using System.Linq;
 using System.Collections.Generic;
 // See https://aka.ms/new-console-template for more information
+/**
+ * <summary>This console program implements the Faro Shuffle technique (shuffling cards)</summary>
+ */
 var startingDeck = from s in Suits()
                    from r in Ranks()
                    select new { Suit = s, Rank = r };
-foreach(var card in startingDeck)
+
+/**
+ * <summary>The above code is equivalent to the below code</summary>
+ * <code>
+ * var startingDeck = Suits()
+ *  .SelectMany(suit => Ranks()
+ *  .Select(rank => new { Suit = suit, Rank = rank }));
+ * </code>
+ */
+
+
+
+
+foreach (var card in startingDeck)
 {
     Console.WriteLine(card);
 }
@@ -32,4 +48,22 @@ static IEnumerable<string> Ranks()
     yield return "queen";
     yield return "king";
     yield return "ace";
+}
+
+/**
+ * <summary>Split the deck into two halves, each having 26 cards</summary>
+ */
+var top = startingDeck.Take(26); // only include the first 26 of the starting Deck
+var bottom = startingDeck.Skip(26); // .... last ....
+
+Console.WriteLine("top");
+foreach (var card in top)
+{
+    Console.WriteLine(card);
+}
+
+Console.WriteLine("bottom");
+foreach (var card in bottom)
+{
+    Console.WriteLine(card);
 }
